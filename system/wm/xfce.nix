7808c5745cd7bf09ad6768e9eb134068650d4faf
edensiiln <siiln@protonmail.com>
2025-04-21 01:36:18 -0500

    new xfce displaymanager

--- system/wm/xfce.nix
+++ system/wm/xfce.nix
@@ -11,33 +11,33 @@
       xfce.enable = true;
     };
   };
-
-  services.greetd = {
-    enable = true;
-    settings = {
-      default_session.command = ''
-             ${pkgs.greetd.tuigreet}/bin/tuigreet \
-        --time \
-        --asterisks \
-        --user-menu \
-        --xsession-wrapper xfce
-      '';
-    };
-  };
-  environment.etc."greetd/environments".text = ''
-    startxfce4
-    zsh
-  '';
-
-  # https://www.reddit.com/r/NixOS/comments/u0cdpi/tuigreet_with_xmonad_how/
-  systemd.services.greetd.serviceConfig = {
-    Type = "idle";
-    StandardInput = "tty";
-    StandardOutput = "tty";
-    StandardError = "journal"; # Without this errors will spam on screen
-    # Without these bootlogs will spam on screen
-    TTYReset = true;
-    TTYVHangup = true;
-    TTYVTDisallocate = true;
-  };
+  services.displayManager.defaultSession = "xfce";
+  #services.greetd = {
+  #  enable = true;
+  #  settings = {
+  #    default_session.command = ''
+  #           ${pkgs.greetd.tuigreet}/bin/tuigreet \
+  #      --time \
+  #      --asterisks \
+  #      --user-menu \
+  #      --xsession-wrapper xfce
+  #    '';
+  #   };
+  #};
+  #environment.etc."greetd/environments".text = ''
+  #  startxfce4
+  #  zsh
+  #'';
+  #
+  ## https://www.reddit.com/r/NixOS/comments/u0cdpi/tuigreet_with_xmonad_how/
+  #systemd.services.greetd.serviceConfig = {
+  #  Type = "idle";
+  #  StandardInput = "tty";
+  #  StandardOutput = "tty";
+  #  StandardError = "journal"; # Without this errors will spam on screen
+  #  # Without these bootlogs will spam on screen
+  #  TTYReset = true;
+  #  TTYVHangup = true;
+  #  TTYVTDisallocate = true;
+  #};
 }
