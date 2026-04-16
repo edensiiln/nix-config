8ae8530493878b4ef93f74e59bf210b4a48815a0
edensiiln <siiln@protonmail.com>
2026-04-16 11:25:05 -0500

    update

---
+++ home/hypr/hypridle.nix
@@ -0,0 +1,28 @@
+{
+  lib,
+  config,
+  ...
+}:
+{
+  options = {
+    hypridleModule.enable = lib.mkEnableOption "enable hypridle";
+  };
+
+  config = lib.mkIf config.hypridleModule.enable {
+    services.hypridle = {
+      enable = true;
+      settings = {
+        general = {
+          after_sleep_cmd = "hyprctl dispatch dpms on";
+        };
+        listener = [
+          {
+	    timeout = 600;
+	    on-timeout = "hyprctl dispatch dpms off";
+	    on-resume = "hyprctl dispatch dpms on && brightnessctl -r";
+	  }
+        ];
+      };
+    };
+  };
+}
